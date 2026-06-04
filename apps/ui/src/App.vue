a292f2fa5b78459feac86ca177c7c3e7779ced12
Michal Průcha <michal.prucha80@gmail.com>
2026-06-04 10:47:51 +0200

    shadcn, tailwind, sonner, button components

---
+++ apps/ui/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import Button from './components/ui/button/Button.vue';
+import { toast } from 'vue-sonner'
+
+import 'vue-sonner/style.css'
+import { Toaster } from '@/components/ui/sonner'
+
+</script>
+
+<template>
+  <h1 class="text-3xl">You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+  <Button  @click="() => toast('My first toast')">Click me</Button>
+  <Toaster />
+</template>
+
+<style scoped></style>
